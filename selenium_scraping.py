d360372513937e86a8be2f98a375069f418300e5
lucas-a-correa <120389170+lucas-a-correa@users.noreply.github.com>
2022-12-28 08:40:49 -0300

    Adição de comentários e correção de formatação

--- selenium_scraping.py
+++ selenium_scraping.py
@@ -1,24 +1,32 @@
+import time
+
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
-import time
-import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
-
+#inicia o webdriver selenium
+site = 'https://www.zapimoveis.com.br'
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 s=Service(r"C:\Users\lucas\Downloads\chromedriver\chromedriver.exe")
 browser = webdriver.Chrome(service=s,options=chrome_options)
-browser.get('https://www.zapimoveis.com.br')
+browser.get(site)
 
-rent_button = browser.find_element(By.XPATH,'//button[@class="new-l-button new-l-button--appearance-outline new-l-button--context-secondary new-l-button--size-regular new-l-button--icon-left new-l-radio-group__button"]')
+#Seleciona as opções de aluguel, imóveis residenciais, filtra pela cidade do rio e efetua a pesquisa
+rent_button = browser.find_element(
+    By.XPATH,'//button[@class="new-l-button new-l-button--appearance-outline new-l-button--context-secondary'\
+    'new-l-button--size-regular new-l-button--icon-left new-l-radio-group__button"]'
+)
 rent_button.click()
 
-type_button = browser.find_element(By.XPATH,'//input[@class="new-l-input__input new-l-input__input--variant-regular new-l-input__input--icon-right"]')
+type_button = browser.find_element(
+    By.XPATH,'//input[@class="new-l-input__input new-l-input__input--variant-regular new-l-input__input--icon-right"]'
+)
 type_button.click()
 
 residential_button = browser.find_element(By.XPATH,'//input[@value="RESIDENTIAL"]')
@@ -36,12 +44,18 @@ select_button = browser.find_element(By.XPATH,'//button[@class="new-l-tag new-l-
 select_button.click()
 select_button.click()
 
-search_button = browser.find_element(By.XPATH,'//button[@class="new-l-button new-l-button--context-primary new-l-button--size-regular new-l-button--icon-left"]')
+search_button = browser.find_element(
+    By.XPATH,'//button[@class="new-l-button new-l-button--context-primary'\
+    'new-l-button--size-regular new-l-button--icon-left"]'
+)
 search_button.click()
 
+#loop que aguarda a página carregar, extrai todas as informações,
+#salva as informações em um DataFrame, concatena no df principal, desce a página e passa para a próxima página
 rent_df = pd.DataFrame()
+pages_n = 200
 
-for i in range (1,200,1):
+for i in range (1,pages_n,1):
     rent_list = list()
     time.sleep(4)
 
@@ -60,5 +74,5 @@ for i in range (1,200,1):
     next_page_button.click()
     print(i)
 
-
-rent_df.to_csv(r'C:\Users\lucas\Downloads\rent.csv',index=False,encoding='UTF-8')
+#salva as informações em um arquivo csv
+rent_df.to_csv(r'.\rent.csv',index=False,encoding='UTF-8')
